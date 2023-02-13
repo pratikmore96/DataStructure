7092098b491c71349a7cc395f0f94a9c7dc81f07
Pratik More <pratikpmore1@gmail.com>
2023-02-13 14:36:00 -0500

    tree

--- tree.py
+++ tree.py
@@ -1,38 +1,61 @@
 class Tree:
 
-    def __init__(self,data) -> None:
+    def __init__(self,data):
         self.data = data
         self.children = []
         self.parent = None
 
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+
+        return level
+ 
+    def __str__(self):
+        print("dddd")
+    def __repr__(self): 
+        pass
+    def __
+    def printtree(self):
+        # # print(self.parent)
+        # print(self.data)
+        # # print(self.children)
+        # if self.children:
+        #     for child in self.children:
+        #         child.print_tree()
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|__" if self.parent else ""
+        print(prefix + self.data)
+        if self.children:
+            for child in self.children:
+                child.printtree()
+
     def create_child(self, child):
         child.parent = self
-        self.children.append(child)
-
-    def printtree(self):
-        print(self.parent)
-        print(self.data)
-        print(self.children)
-        
+        self.children.append(child)        
 
 def create_hierarchy():
     
     root = Tree("electronic")
 
-    laptop = Tree("laptop")
-    laptop.create_child("hp")
-    laptop.create_child("samsung")
-    laptop.create_child("lenovo")
+    laptop = Tree("Laptop")
+    laptop.create_child(Tree("hp"))
+    laptop.create_child(Tree("samsung"))
+    laptop.create_child(Tree("lenovo"))
 
-    tv = Tree("laptop")
-    tv.create_child("hp")
-    tv.create_child("samsung")
-    tv.create_child("lenovo")
+    tv = Tree("TV")
+    tv.create_child(Tree("sonata"))
+    tv.create_child(Tree("samsung"))
+    tv.create_child(Tree("rocky"))
 
-    root.create_child("laptop")
-    root.create_child("tv")
+    root.create_child(laptop)
+    root.create_child(tv)
 
     root.printtree()
+    print(root)
 
 if __name__ == '__main__':
     create_hierarchy()
